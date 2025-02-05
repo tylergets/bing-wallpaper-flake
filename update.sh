327785c846253a526b0d846a251fa0321aa0ff45
Tyler Getsay <tylergetsay@gmail.com>
2025-02-05 13:08:40 -0500

    chore: update UHD

--- update.sh
+++ update.sh
@@ -4,11 +4,16 @@
 BING_URL="https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US"
 
 RESPONSE=$(curl -s $BING_URL)
-IMAGE_URL_VAL=$(echo "$RESPONSE" | jq -r '.images[0].url')
+IMAGE_URL_VAL=$(echo "$RESPONSE" | jq -r '.images[0].urlbase')
 IMAGE_URL="https://www.bing.com$IMAGE_URL_VAL"
 
 IMAGE_FILE="bing_image.jpg"
 JSON_FILE="image_data.json"
+
+APPEND_DATA="_UHD.jpg&rf=LaDigue_UHD.jpg&pid=hp"
+
+IMAGE_URL="${IMAGE_URL}${APPEND_DATA}"
+echo "Downloading image from $IMAGE_URL"
 
 # Download the image
 curl -s -o $IMAGE_FILE $IMAGE_URL
